701da1f36fe77b65780dde6088733a8e54febf1d
zxstudio-llc <itsupport@zx-studio.com>
2024-05-17 11:52:39 -0500

    update project

--- src/transroles/dto/update-transrole.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTransroleDto } from './create-transrole.dto';
-
-export class UpdateTransroleDto extends PartialType(CreateTransroleDto) {}
--- src/transroles/transroles.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TransrolesService } from './transroles.service';
-import { TransrolesController } from './transroles.controller';
-
-@Module({
-  controllers: [TransrolesController],
-  providers: [TransrolesService],
-})
-export class TransrolesModule {}
